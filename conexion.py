cb9840cd5353600332d980bfa4eb868c471be154
Extiben <jhon.mosquerapalacios@inemjose.edu.co>
2025-08-22 17:39:03 -0500

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -4,8 +4,8 @@ def crear_conexion():
     """Crea y retorna una conexión a la base de datos"""
     conn = mysql.connector.connect(
         host="localhost",
-        user="root",       # Tu usuario de MySQL/phpMyAdmin
-        password="",       # Tu contraseña (si no tienes, déjalo vacío)
+        user="root",       
+        password="",      
         database="biblioteca"
     )
     return conn
